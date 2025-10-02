6d79dec97b72b1753a19efbd7ee9bb1893e5cb6f
mattschiller <itsmattjs@gmail.com>
2025-10-02 12:24:04 -0700

    first commit

---
+++ terraform/auth0-tenant/main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    auth0 = {
+      source  = "auth0/auth0"
+      version = "~> 1.0"
+    }
+  }
+}
+
+provider "auth0" {
+  domain        = var.auth0_domain
+  client_id     = var.auth0_client_id
+  client_secret = var.auth0_client_secret
+}
+
+# Basic Auth0 Tenant - just the essentials
+resource "auth0_tenant" "main" {
+  friendly_name = "tes"
+}
+
