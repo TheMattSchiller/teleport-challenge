ad1446baa81f88701b786513bf4a371058c14323
mattschiller <itsmattjs@gmail.com>
2025-10-02 12:24:53 -0700

    Keep auth0-tenant-setup.yml during rebase

---
+++ terraform/auth0-tenant/variables.tf
@@ -0,0 +1,19 @@
+variable "auth0_domain" {
+  description = "Auth0 domain"
+  type        = string
+  default       = "dev-i7byu1nxmf5j3pj4.us.auth0.com"
+}
+
+variable "auth0_client_id" {
+  description = "Auth0 client ID"
+  type        = string
+  default       = "kVfvBeMccQ0Y3MMpARq6ZRR6PCmV6z2J"
+}
+
+variable "auth0_client_secret" {
+  description = "Auth0 client secret"
+  type        = string
+  sensitive   = true
+}
+
+
---
+++ terraform/aws-grafana/main.tf
@@ -0,0 +1,106 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
+# Data sources
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
+
+data "aws_caller_identity" "current" {}
+
+# Security Group for Grafana
+resource "aws_security_group" "grafana_sg" {
+  name_prefix = "grafana-"
+  description = "Security group for Grafana"
+  vpc_id      = data.aws_vpc.default.id
+
+  ingress {
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Grafana"
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "All outbound traffic"
+  }
+
+  tags = {
+    Name = "grafana-security-group"
+  }
+}
+
+# ECS Cluster
+resource "aws_ecs_cluster" "grafana_cluster" {
+  name = "grafana-cluster"
+}
+
+# ECS Task Definition
+resource "aws_ecs_task_definition" "grafana_task" {
+  family                   = "grafana"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = 256
+  memory                   = 512
+
+  container_definitions = jsonencode([
+    {
+      name  = "grafana"
+      image = "grafana/grafana:latest"
+      portMappings = [
+        {
+          containerPort = 3000
+          protocol      = "tcp"
+        }
+      ]
+      environment = [
+        {
+          name  = "GF_SECURITY_ADMIN_USER"
+          value = "admin"
+        },
+        {
+          name  = "GF_SECURITY_ADMIN_PASSWORD"
+          value = "admin"
+        }
+      ]
+    }
+  ])
+}
+
+# ECS Service
+resource "aws_ecs_service" "grafana_service" {
+  name            = "grafana-service"
+  cluster         = aws_ecs_cluster.grafana_cluster.id
+  task_definition = aws_ecs_task_definition.grafana_task.arn
+  desired_count   = 1
+  launch_type     = "FARGATE"
+
+  network_configuration {
+    subnets          = data.aws_subnets.default.ids
+    security_groups  = [aws_security_group.grafana_sg.id]
+    assign_public_ip = true
+  }
+}
+
